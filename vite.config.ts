d0d4a3582314c468a5b137467a8b15d501d8906d
utsav-bhandari <145168913+utsav-bhandari@users.noreply.github.com>
2026-04-30 21:17:58 -0500

    created a landing page as per project requirement

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-    base: "/thr33craft",
     plugins: [tsconfigPaths()],
     build: {
         chunkSizeWarningLimit: 5000,
