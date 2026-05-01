90869c3cbeddf6dfbfe182c04be80c58d92479be
utsav-bhandari <145168913+utsav-bhandari@users.noreply.github.com>
2026-04-30 21:19:09 -0500

    minor script fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+// @ts-ignore
+import { resolve } from "node:path";
+
 export default defineConfig({
     plugins: [tsconfigPaths()],
     build: {
         chunkSizeWarningLimit: 5000,
+        rollupOptions: {
+            input: {
+                // @ts-ignore
+                main: resolve(__dirname, "index.html"),
+                // @ts-ignore
+                landing: resolve(__dirname, "landing.html"),
+            },
+        },
     },
+    appType: "mpa",
 });
